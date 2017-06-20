278ca78bb5d2674b41c5bd78d62eca41a8632436
hzmeric@163.com <hzmeric@163.com>
2017-06-20 15:00:56 +0800

    hzm

--- src/main/java/com/mycompany/mavenproject1/Demo.java
+++ src/main/java/com/mycompany/mavenproject1/Demo.java
@@ -13,6 +13,9 @@ package com.mycompany.mavenproject1;
 public class Demo {
     public static void main(String[] args ){
      System.out.println( Singleton.getIntance());
+        
+     
+     
      
     }
 }
